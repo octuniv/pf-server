8800bb7f2246c078b4fb7010618787e04ac5c2bf
octuniv <mayday010771@gmail.com>
2024-09-03 10:36:44 +0900

    1. save port to open server in env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(process.env.HTTP_SERVER_POST);
 }
 bootstrap();
